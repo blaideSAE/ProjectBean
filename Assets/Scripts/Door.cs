5d7a52ece890da096e2fe5838a52fd06deee85f3
Blaide <1017007@student.sae.edu.au>
2020-04-22 15:01:16 +1000

    Added Door Moved stuff around in scene, configured spawner

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Vector3 OpenPosition;
+    private Vector3 startPosition;
+    public float moveTime;
+    public AnimationCurve moveCurve;
+
+    private Bounds bounds;
+
+    private bool doorOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggle()
+    {
+        if (doorOpen)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        transform.DOMove(transform.position + OpenPosition, moveTime).SetEase(moveCurve)
+            .OnComplete((() => doorOpen = true));
+    }
+
+    public void Close()
+    {
+        transform.DOMove(startPosition, moveTime).SetEase(moveCurve)
+            .OnComplete((() => doorOpen = false));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 boundsOffset = new Vector3(bounds.extents.x, -bounds.extents.y, bounds.extents.z);
+        if (startPosition == Vector3.zero)
+        {
+            bounds = GetComponent<Collider>().bounds;
+            Gizmos.DrawWireCube( transform.position + OpenPosition - boundsOffset,bounds.size);
+        }
+        else
+        {
+            
+            bounds = GetComponent<Collider>().bounds;
+            Gizmos.DrawWireCube( startPosition + OpenPosition - boundsOffset,bounds.size);
+        }
+
+    }
+}
